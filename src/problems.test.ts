014d477f2eaec21b66ea01ec1238d6f4fca9073f
Luke Massa <lmassa@tripadvisor.com>
2026-02-01 00:05:48 -0500

    Add more validation

--- src/problems.test.ts
+++ src/problems.test.ts
@@ -3,7 +3,7 @@ import * as assert from "node:assert/strict";
 import { findProblemDates } from "./problems";
 
 test.test("detects tuesday Jan 31", () => {
-  const res = findProblemDates("Meet me Tuesday Jan 31.");
+  const res = findProblemDates("Meet me Tuesday Jan 31");
   assert.ok(res);
   assert.equal(res.getTime(), new Date(2026, 0, 31).getTime());
 });
@@ -13,14 +13,38 @@ test.test("allow for numbers after Tuesday Jan 1", () => {
   assert.equal(res, null);
 });
 
+test.test("do not allow for numbers after Tuesday Jan 11", () => {
+  const res = findProblemDates("Meet me Tuesday Jan 11");
+  assert.ok(res);
+  assert.equal(res.getTime(), new Date(2026, 0, 11).getTime());
+});
+
 test.test("after a comma know the date is over", () => {
-  const res = findProblemDates("Meet me Tuesday Jan 1, if you cant");
+  const res = findProblemDates("Meet me Tuesday Jan 1, if you can");
   assert.ok(res);
   assert.equal(res.getTime(), new Date(2026, 0, 1).getTime());
 });
 
 test.test("detects saturday Jan 31", () => {
   const res = findProblemDates("Meet me Saturday Jan 31.");
+  assert.equal(res, null);
+});
+
+test.test("detects problem after valid date", () => {
+  const res = findProblemDates(
+    "Meet me Saturday Jan 31. Then we can discuss this on Monday, Feb 1.",
+  );
+  assert.ok(res);
+  assert.equal(res.getTime(), new Date(2026, 1, 1).getTime());
+});
+
+test.test("handles 0 date", () => {
+  const res = findProblemDates("Meet me Saturday Jan 0.");
+  assert.equal(res, null);
+});
+
+test.test("handles 99 date", () => {
+  const res = findProblemDates("Meet me Saturday Jan 99.");
   assert.equal(res, null);
 });
 
--- src/problems.ts
+++ src/problems.ts
@@ -1,25 +1,35 @@
-const dateRegex =
-  /(mon|monday|tue|tues|tuesday|wed|wednesday|thu|thurs|thursday|fri|friday|sat|saturday|sun|sunday),?\s+(jan|january|feb|february|mar|march|apr|april|may|jun|june|jul|july|aug|august|sep|sept|september|oct|october|nov|november|dec|december)\s+([1-9][0-9]?)(?:[^\d])/i;
+const dateRegexFactory = () =>
+  /(mon|monday|tue|tues|tuesday|wed|wednesday|thu|thurs|thursday|fri|friday|sat|saturday|sun|sunday),?\s+(jan|january|feb|february|mar|march|apr|april|may|jun|june|jul|july|aug|august|sep|sept|september|oct|october|nov|november|dec|december)\s+([1-9][0-9]?)/gi;
 const year = 2026; // TODO: figure this out dynamically
 
 export function findProblemDates(text: string): Date | null {
-  const match = dateRegex.exec(text);
-  if (match === null) {
-    return null;
+  let dateRegex = dateRegexFactory();
+  let match: RegExpExecArray | null;
+  while ((match = dateRegex.exec(text)) !== null) {
+    const month = match[2].slice(0, 3);
+    const day = match[3];
+
+    const matchEndsAtEndOfInput = match.index + match[0].length === text.length;
+    // If the number is 1 and 2, and we're at the end of the string, it's possible the user will type more so leave it.
+    // If they started to type a 3, or they've already typed 11, then we don't need to skip, we have the full date
+    if (parseInt(day) < 3 && matchEndsAtEndOfInput) {
+      continue;
+    }
+    const date = new Date(`${month} ${day}, ${year}`);
+    if (isNaN(date.getTime())) {
+      continue;
+    }
+
+    const actualWeekday = date
+      .toLocaleDateString("en-US", { weekday: "short" })
+      .toLowerCase();
+
+    const foundWeekday = match[1].slice(0, 3).toLowerCase();
+
+    if (foundWeekday === actualWeekday) {
+      continue;
+    }
+    return date;
   }
-  const month = match[2].slice(0, 3);
-  const day = match[3];
-
-  const date = new Date(`${month} ${day}, ${year}`);
-
-  const actualWeekday = date
-    .toLocaleDateString("en-US", { weekday: "short" })
-    .toLowerCase();
-
-  const foundWeekday = match[1].slice(0, 3).toLowerCase();
-
-  if (foundWeekday === actualWeekday) {
-    return null;
-  }
-  return date;
+  return null;
 }
